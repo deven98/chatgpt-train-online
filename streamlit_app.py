6789b569827879790c02e5a5b86996f306142c54
Deven Joshi <deven9852@gmail.com>
2023-03-19 21:52:00 +0530

    added customisation options

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,12 @@ import pandas as pd
 import streamlit as st
 
 st.write('# Fine-tune an OpenAI model online')
+
+api_key = st.text_input('OpenAPI Secret Key')
+
+model_name = st.text_input('Model name')
+
+option = st.selectbox(
+    'Select model type',
+    ('davinci', 'curie', 'babbage', 'ada'))
+
